f2fc09279c864dde814c162f6dfd87b59291c75e
Chris F <CF-public@gmx.net>
2022-12-01 18:02:23 +0100

    Reworked "ContentFinder<Texture2D>.Get"-PostFix-patch into "Graphic_Multi.Init"-Transpiler; the former applied to all generic "ContentFinder.Get"-calls apparently, hopefully "Graphic_Multi.Init" is used in all cases where the pack-texture-patch is necessary

--- Source/CustomizeAnimals/Main/HarmonyPatches.cs
+++ Source/CustomizeAnimals/Main/HarmonyPatches.cs
@@ -7,6 +7,9 @@ using Verse;
 using HarmonyLib;
 using RimWorld;
 using UnityEngine;
+using Mono.Cecil.Cil;
+using System.Reflection.Emit;
+using System.Reflection;
 
 namespace CustomizeAnimals
 {
@@ -15,7 +18,7 @@ namespace CustomizeAnimals
 	{
 		static HarmonyPatches()
 		{
-			Harmony harmony = new Harmony("syrus.customize_animals");
+			var harmony = new Harmony("syrus.customize_animals");
 
 			harmony.Patch(
 				AccessTools.Method(typeof(TrainableUtility), nameof(TrainableUtility.DegradationPeriodTicks)),
@@ -26,8 +29,8 @@ namespace CustomizeAnimals
 				postfix: new HarmonyMethod(typeof(HarmonyPatches), nameof(HarmonyPatches.MassUtility_Capacity_PostFix)));
 
 			harmony.Patch(
-				AccessTools.Method(typeof(ContentFinder<Texture2D>), nameof(ContentFinder<Texture2D>.Get)),
-				postfix: new HarmonyMethod(typeof(HarmonyPatches), nameof(HarmonyPatches.ContentFinder_Texture2D_Get_PostFix)));
+				AccessTools.Method(typeof(Graphic_Multi), nameof(Graphic_Multi.Init)),
+				transpiler: new HarmonyMethod(typeof(HarmonyPatches), nameof(HarmonyPatches.Graphic_Multi_Init_Transpiler)));
 		}
 
 		public static void TrainableUtility_DegradationPeriodTicks_PostFix(ref int __result)
@@ -42,13 +45,64 @@ namespace CustomizeAnimals
 				__result *= p.def.statBases.GetStatValueFromList(StatDefOf.CarryingCapacity, StatDefOf.CarryingCapacity.defaultBaseValue) / StatDefOf.CarryingCapacity.defaultBaseValue;
 		}
 
-		public static void ContentFinder_Texture2D_Get_PostFix(string itemPath, ref Texture2D __result)
+		public static IEnumerable<CodeInstruction> Graphic_Multi_Init_Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			var patched = false;
+			var list = instructions.ToList();
+			for (int i = 4; i < list.Count; i++)
+			{
+				// 4  ldfld System.String Verse.GraphicRequest::path
+				// 3  ldc.i4.0 NULL
+				// 2  call static UnityEngine.Texture2D Verse.ContentFinder`1<UnityEngine.Texture2D>::Get(System.String itemPath, System.Boolean reportFailure)
+				// 1  stelem.ref NULL
+				//  + ldloc.0 NULL
+				//  + ldc.i4.0 NULL
+				//  + ldelema UnityEngine.Texture2D
+				//  + ldarg.1 NULL
+				//  + ldfld System.String Verse.GraphicRequest::path
+				//  + call static System.Void CustomizeAnimals.HarmonyPatches::Patchy(UnityEngine.Texture2D& texture2D, System.String path)
+				// 0  ldloc.0 NULL [Label0, Label2, Label4, Label6]
+				if (   list[i - 4].opcode == OpCodes.Ldfld
+					&& list[i - 4].operand is FieldInfo fieldInfo
+					&& fieldInfo.DeclaringType == typeof(GraphicRequest)
+					&& fieldInfo.Name == nameof(GraphicRequest.path)
+					&& list[i - 3].opcode == OpCodes.Ldc_I4_0
+					&& list[i - 2].opcode == OpCodes.Call
+					&& list[i - 2].operand is MethodInfo methodInfo
+					&& methodInfo.IsStatic
+					&& methodInfo.DeclaringType == typeof(ContentFinder<Texture2D>)
+					&& methodInfo.Name == nameof(ContentFinder<Texture2D>.Get)
+					&& list[i - 1].opcode == OpCodes.Stelem_Ref
+					&& list[i].opcode == OpCodes.Ldloc_0
+					&& list[i].labels.Count == 4)
+				{
+					list.InsertRange(i, new CodeInstruction[]
+					{
+						new CodeInstruction(OpCodes.Ldloc_0),
+						new CodeInstruction(OpCodes.Ldc_I4_0),
+						new CodeInstruction(OpCodes.Ldelema, typeof(Texture2D)),
+						new CodeInstruction(OpCodes.Ldarg_1),
+						new CodeInstruction(OpCodes.Ldfld, typeof(GraphicRequest).GetField(nameof(GraphicRequest.path))),
+						new CodeInstruction(OpCodes.Call, typeof(HarmonyPatches).GetMethod(nameof(HarmonyPatches.Graphic_Multi_Init_PackTexture_Patch), BindingFlags.Static | BindingFlags.NonPublic)),
+					});
+					patched = true;
+					break;
+				}
+			}
+
+			if (!patched)
+				Log.Error($"{nameof(CustomizeAnimals)}: failed to apply '{nameof(Graphic_Multi_Init_Transpiler)}'-patch");
+
+			return list;
+		}
+
+		private static void Graphic_Multi_Init_PackTexture_Patch(ref Texture2D texture2D, string path)
 		{
 			const string dummy = "CA_pack_dummy";
-			if (__result == null && itemPath.EndsWith("Pack"))
+			if (texture2D == null && path.EndsWith("Pack"))
 			{
-				__result = CustomizeAnimals.Instance.Content.GetContentHolder<Texture2D>().Get(dummy);
-				Log.Warning($"{nameof(CustomizeAnimals)}: Pack animal texture not found: {itemPath} - replacing with dummy: {dummy} - success: {__result != null}");
+				texture2D = CustomizeAnimals.Instance.Content.GetContentHolder<Texture2D>().Get(dummy);
+				Log.Warning($"{nameof(CustomizeAnimals)}: pack animal texture not found: {path} - replacing with dummy: {dummy} - success: {texture2D != null}");
 			}
 		}
 	}
